2e923052cdb8e95aa4696970d89d0e6f79610571
Ngoc Nguyen <nnpk0410@gmail.com>
2023-04-13 08:37:51 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userData = {
+        name: userName,
+        email: userEmail,
+    }    
+
+    const handleUserData = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData)
+    }
+
+    return fetch("http://localhost:3000/users", handleUserData)
+        .then((res) => res.json())
+        .then((data) => {
+            document.body.innerHTML = data.id
+        })
+        .catch((error) => {
+            const errorMessage = document.createElement("p");
+
+            errorMessage.textContent = error.message;
+            
+            document.body.appendChild(errorMessage);
+        })
+}
+
